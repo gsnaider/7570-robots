59c23fd875e5c27f0844acfc2e12e32173133a5b
Gaston Snaider <gnsnaider@gmail.com>
2018-09-02 17:40:21 -0700

    Graph created

--- gan/gan.py
+++ gan/gan.py
@@ -14,8 +14,16 @@ TRAINING_DATA_DIR = os.path.join("data", "mnist_png", "training")
 # the generator
 BATCH_SIZE = 60
 
+GEN_LEARNING_RATE = 0.1
+DISC_LEARNING_RATE = 0.1
+
 GEN_HIDDEN_LAYERS = [20, 20, 20, 20, 20]
 DISC_HIDDEN_LAYERS = [20, 20, 20, 20, 20]
+
+LATENT_SPACE_SHAPE = 100
+
+GEN_VARIABLE_SCOPE = "generator"
+DISC_VARIABLE_SCOPE = "discriminator"
 
 
 def generator(latent_space):
@@ -26,13 +34,14 @@ def generator(latent_space):
     Returns:
         Generated images
     """
-    net = latent_space
-    for layer in GEN_HIDDEN_LAYERS:
-        net = tf.layers.dense(net, layer, activation=tf.nn.relu)
+    with tf.variable_scope(GEN_VARIABLE_SCOPE):
+        net = latent_space
+        for layer in GEN_HIDDEN_LAYERS:
+            net = tf.layers.dense(net, layer, activation=tf.nn.relu)
 
-    output = tf.layers.dense(net, 28 * 28, activation=tf.nn.sigmoid)
-    images = tf.reshape(output, [None, 28, 28])
-    return images
+        output = tf.layers.dense(net, 28 * 28, activation=tf.nn.sigmoid)
+        images = tf.reshape(output, [-1, 28, 28])
+        return images
 
 
 def discriminator(images):
@@ -40,15 +49,16 @@ def discriminator(images):
     Args:
         images: input images as 28x28 tensors
     Returns:
-        Logits and classification for each image
+        Logits and prediction for each image
     """
-    net = tf.reshape(images, [None, 28 * 28])
-    for layer in DISC_HIDDEN_LAYERS:
-        net = tf.layers.dense(net, layer, activation=tf.nn.relu)
+    with tf.variable_scope(DISC_VARIABLE_SCOPE, reuse=tf.AUTO_REUSE):
+        net = tf.reshape(images, [-1, 28 * 28])
+        for layer in DISC_HIDDEN_LAYERS:
+            net = tf.layers.dense(net, layer, activation=tf.nn.relu)
 
-    logits = tf.layers.dense(net, 1)
-    classification = tf.nn.sigmoid(logits)
-    return logits, classification
+        logits = tf.layers.dense(net, 1)
+        prediction = tf.nn.sigmoid(logits)
+        return logits, prediction
 
 
 def _parse_function(filename, label):
@@ -72,9 +82,12 @@ def _mnist_filenames_and_labels():
     for label in range(10):
         images_dir = os.path.join(TRAINING_DATA_DIR, str(label))
         current_images_paths = os.listdir(images_dir)
-        images_paths += list(map(lambda image_path : os.path.join(images_dir, image_path), current_images_paths))
+        images_paths += list(
+            map(lambda image_path: os.path.join(images_dir, image_path),
+                current_images_paths))
         images_labels += [label] * len(current_images_paths)
     return images_paths, images_labels
+
 
 def shuffle(a, b):
     assert len(a) == len(b)
@@ -85,22 +98,52 @@ def shuffle(a, b):
 filenames, labels = _mnist_filenames_and_labels()
 filenames, labels = shuffle(np.array(filenames), np.array(labels))
 
+step = tf.train.get_or_create_global_step()
+increment_step = tf.assign(step, step + 1)
+
 dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
 dataset = dataset.map(_parse_function)
 dataset = dataset.shuffle(buffer_size=1000)
 
 # The other half of the batch will come from the generator.
-dataset = dataset.batch(batch_size= BATCH_SIZE // 2)
+dataset = dataset.batch(batch_size=BATCH_SIZE // 2)
 
 dataset = dataset.repeat()
 
 iterator = dataset.make_one_shot_iterator()
+
+# Iterator for tuples of images and labels
 next = iterator.get_next()
 
-with tf.Session() as sess:
-    images, labels = sess.run(next)
-    for i in range(10):
-        image = images[i]
-        print(labels[i])
-        plt.imshow(image, cmap='gray')
-        plt.show()
+latent_space = tf.placeholder(tf.float32, shape=[None, LATENT_SPACE_SHAPE])
+G_images = generator(latent_space)
+
+D_fake_logits, D_fake_pred = discriminator(G_images)
+D_real_logits, D_real_pred = discriminator(next[0])
+
+G_expected = tf.zeros_like(D_fake_logits)
+G_loss = tf.losses.sigmoid_cross_entropy(G_expected, D_fake_logits)
+
+D_real_expected = tf.zeros_like(D_real_logits)
+D_fake_expected = tf.ones_like(D_fake_logits)
+
+D_real_loss = tf.losses.sigmoid_cross_entropy(D_real_expected, D_real_logits)
+D_fake_loss = tf.losses.sigmoid_cross_entropy(D_fake_expected, D_fake_logits)
+D_loss = D_real_loss + D_fake_loss
+
+G_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                GEN_VARIABLE_SCOPE)
+
+print("G_variables ", G_variables)
+
+G_optimizer = tf.train.GradientDescentOptimizer(
+    learning_rate=GEN_LEARNING_RATE).minimize(G_loss, var_list=G_variables)
+
+D_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                DISC_VARIABLE_SCOPE)
+
+print("D_variables ", D_variables)
+
+D_optimizer = tf.train.GradientDescentOptimizer(
+    learning_rate=DISC_LEARNING_RATE).minimize(D_loss, var_list=D_variables)
+
